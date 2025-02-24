65f2cbd41511b385bd067e3dd406cae4639e2158
KimJiMi <teameod417@gmail.com>
2025-02-25 06:32:52 +0900

    main.py 업데이트

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse, FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, 개지미스!"}
+    return JSONResponse(
+        content={"message": "Hello, 개지미스!"},
+        media_type="application/json; charset=utf-8"
+    )
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
