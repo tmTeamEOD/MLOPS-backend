2dda3c82cc4c21cf2f9c43351856837ae897f238
tmteameod <skitterbot@test.com>
2025-02-25 08:03:43 +0900

    테스트 테이블

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return JSONResponse(
-        content={"message": "Hello,태스트!"},
+        content={"message": "Hello,디비 테스트!"},
         media_type="application/json; charset=utf-8"
     )
 
