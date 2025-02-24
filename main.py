95033e70e698d592005b6fdec55896f06104df61
tmteameod <skitterbot@test.com>
2025-02-25 06:23:46 +0900

    k8s auto docker

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, 지미카ㅇ 터ㅇㅅ ㅇㄴㄴ피슝!"}
+    return {"message": "Hello, 개지미스!"}
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
