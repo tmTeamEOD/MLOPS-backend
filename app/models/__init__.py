5feb660b59107d3f7859db94444b8244e16c6d3f
tmteameod <skitterbot@test.com>
2025-02-25 07:58:53 +0900

    테스트 테이블

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,11 @@
-from app.models.base import Base
+from app.models.base import Base, TestTable
+
 # from app.models.user import User
 # from app.models.item import Item
 
 # 여기에 모든 모델을 import 해서 alembic이 자동으로 인식할 수 있게 함
 __all__ = ["Base",
+           "TestTable"
            # "User",
            # "Item"
            ]
--- app/models/base.py
+++ app/models/base.py
@@ -1,4 +1,13 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+# 테스트용 테이블 추가
+class TestTable(Base):
+    __tablename__ = "test_table"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(255), nullable=True)
