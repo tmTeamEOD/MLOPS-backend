38e7927081977f5ba3ac1aba8c1e2f29915e1fd3
KimJiMi <teameod417@gmail.com>
2025-02-25 09:24:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return JSONResponse(
-        content={"message": "Hello,디비 테스트56!"},
+        content={"message": "Hello,디비 테스트5611!"},
         media_type="application/json; charset=utf-8"
     )
 
