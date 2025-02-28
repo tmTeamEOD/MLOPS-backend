0b8bd6592818d851754f3a3b7d52985bce23eae5
Jimi Kim <kimwhad@gmail.com>
2025-02-28 09:37:13 +0900

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return JSONResponse(
-        content={"message": "Test this!"},
+        content={"message": "Test this2!"},
         media_type="application/json; charset=utf-8"
     )
 
